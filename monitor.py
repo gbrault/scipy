8eb0b11b8b18cc7cacceb2c6e5859a358a889387
Gilbert Brault <gbrault@seadev.org>
2020-09-11 12:11:31 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -16,3 +16,7 @@ if jod_url is not None and sleep is not None and token is not None:
         timestamp = datetime.datetime.now().strftime('%s')
         requests.get(jod_url+f"&token={token}&timestamp={timestamp}")
         time.sleep(sleep)
+else:
+    print("JoD monitor not ready to start!")
+    print("Check JOD_URL, JOD_SLEEP environment variables")
+    print(f"Jupyter token: {token}")
