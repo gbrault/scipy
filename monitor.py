95458ea3f1621807eccf7478a996fdebde585b11
Gilbert Brault <gbrault@seadev.org>
2020-09-11 15:20:08 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -8,7 +8,9 @@ import datetime
 jod_url = os.getenv("JOD_URL", None)
 sleep = os.getenv("JOD_SLEEP", None)
 servers = list(notebookapp.list_running_servers())
-token = servers[0]['token']
+token = None
+if servers is not None and len(servers)>0:
+    token = servers[0]['token']
 
 if jod_url is not None and sleep is not None and token is not None:
     sleep = int(sleep)
@@ -19,4 +21,5 @@ if jod_url is not None and sleep is not None and token is not None:
 else:
     print("JoD monitor not ready to start!")
     print("Check JOD_URL, JOD_SLEEP environment variables")
-    print(f"Jupyter token: {token}")
+    if token is not None:
+        print(f"Jupyter token: {token}")
