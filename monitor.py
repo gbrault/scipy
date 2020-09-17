5afec57b5a2dab05a2d3927e3381f946ea64e065
Gilbert Brault <gbrault@seadev.org>
2020-09-17 17:36:00 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -21,27 +21,27 @@ def get_tokens():
                                "started": rjson['started']})
             else:
                 tokens.append({"port": server['port'], "token": server['token']})
-    tokens = json.dumps(tokens)
     return tokens
 
 jod_url = os.getenv("JOD_URL", None)
 sleep = os.getenv("JOD_SLEEP", f"{60*10}")  # defaults to 10 minutes = 60*10 seconds
 sleep = int(sleep)                                
 tokens = get_tokens()
-print(f"Jupyter tokens: {tokens}", flush=True)
+print(f"Jupyter tokens: {json.dumps(tokens)}", flush=True)
 jod_git_url = os.getenv("JOD_GIT_URL", None)
+jod_ak = os.getenv("JOD_AK", None)
 if jod_git_url is not None:
     os.system(f"git -C '/home/jovyan/work' clone {jod_git_url}")
 # Loop forever
 while True:
     timestamp = datetime.datetime.now().strftime('%s')
     tokens = get_tokens()
-    jsondata = {"tokens": tokens, "timestamp": timestamp}
+    jsondata = {"ak": jod_ak, "tokens": tokens, "timestamp": timestamp}
     if jod_url is not None:
         requests.post(jod_url, json=jsondata)
     else:
         print("JoD monitor not ready to start!")
         print("Check JOD_URL, JOD_SLEEP environment variables")
         if tokens is not None:
-            print(f"Jupyter tokens: {tokens}", flush=True)
+            print(f"Jupyter tokens: {json.dumps(tokens)}", flush=True)
     time.sleep(sleep)
