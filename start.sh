18a1d10da92188e94d0f0a61cb14bd33c595cdda
Gilbert Brault <gbrault@seadev.org>
2020-09-18 07:43:35 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 sudo su <<HERE
+echo "JOD_URL=$JOD_URL" > /home/jovyan/.env
+echo "JOD_SLEEP=$JOD_SLEEP" >> /home/jovyan/.env
+echo "JOD_GIT_URL=$JOD_GIT_URL" >> /home/jovyan/.env
+echo "JOD_AK=$JOD_AK" >> /home/jovyan/.env
+chown jovyan:users /home/jovyan/.env
+HERE
 cd /home/jovyan
 /opt/conda/bin/python /home/jovyan/monitor.py
-HERE
