4565d5fdbd1f96d47c47883aa5359b1c6547881e
Gilbert Brault <gbrault@seadev.org>
2020-09-19 13:16:33 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -7,8 +7,14 @@ import datetime
 import json
 from dotenv import load_dotenv
 load_dotenv(dotenv_path='/home/jovyan/.env')
+import dateutil.parser
+import datetime
+
+last_activity = -1
 
 def get_tokens():
+    global last_activity
+    last_activity = -1
     tokens = []
     servers = list(notebookapp.list_running_servers())
     if servers is not None and len(servers)>0:        
@@ -21,6 +27,7 @@ def get_tokens():
                                "token": server['token'],
                                "last_activity": rjson['last_activity'],
                                "started": rjson['started']})
+                last_activity = dateutil.parser.parse(rjson['last_activity'])
             else:
                 tokens.append({"port": server['port'], "token": server['token']})
     return tokens
@@ -29,6 +36,9 @@ jod_url = os.getenv("JOD_URL", None)
 sleep = os.getenv("JOD_SLEEP", f"{60*10}")  # defaults to 10 minutes = 60*10 seconds
 sleep = int(sleep)                                
 tokens = get_tokens()
+if last_activity == -1 or datetime.datetime.now() > last_activity + datetime.timedelta(seconds=sleep):
+    os.system("sudo kill 1")
+    # Terminate the container
 print(f"Jupyter tokens: {json.dumps(tokens)}", flush=True)
 jod_git_url = os.getenv("JOD_GIT_URL", None)
 jod_ak = os.getenv("JOD_AK", None)
